a4bbe8cec9a6ba477f39c210f40c0f8014d75f9c
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-11-04 22:36:15 +0000

    add placeholder

---
+++ components/TBC.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "hono/jsx";
+
+const TBC: FC = () => {
+    return (
+        <div className="mt-24 w-fit mx-auto p-8 text-gray-500 rounded-lg bg-gray-100 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-[hsl(240, 6%, 15%)] dark:hover:bg-[hsl(240, 6%, 13%)] dark:hover:text-white">
+        <p className="text-center text-xl opacity-60">
+            This section is to be completed.
+        </p>
+        </div>
+    )
+}
+
+export default TBC;
