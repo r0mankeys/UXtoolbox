cf9f75989916d9f22aa4f5e8c19ef89e5c2cb74c
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-11-04 23:17:07 +0000

    serve static svg upon deploy

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import { serveStatic } from 'hono/deno'
 import dotenv from "npm:dotenv";
+import { join, dirname } from "https://deno.land/std/path/mod.ts";
 import process from "node:process"
 
 import app from "./index.tsx"
 
 dotenv.config()
 
-app.use('/assets/*', serveStatic({ root: '/assets' }))
+app.use(serveStatic({ root: join(dirname(import.meta.url), "assets") }));
 
 Deno.serve({ hostname: String(process.env.HOST), port: Number(process.env.PORT) }, app.fetch)
