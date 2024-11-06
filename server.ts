accb6bd171a633b0e166d07b2415d2069a85624b
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-11-06 16:46:58 +0000

    update server file to get rid of uncecessary imports

--- server.ts
+++ server.ts
@@ -1,12 +1,7 @@
 import { serveStatic } from 'hono/deno'
-import dotenv from "npm:dotenv";
-import { join, dirname } from "https://deno.land/std/path/mod.ts";
-import process from "node:process"
 
 import app from "./index.tsx"
 
-dotenv.config()
+app.use(serveStatic({ root: "/assets" }))
 
-app.use(serveStatic({ root: join(dirname(import.meta.url), "/assets") }));
-
-Deno.serve({ hostname: String(process.env.HOST), port: Number(process.env.PORT) }, app.fetch)
+Deno.serve(app.fetch)
