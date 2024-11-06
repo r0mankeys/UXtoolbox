39c37fa38b691af0e4d5204bede83ec03c97fd21
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-11-06 17:05:32 +0000

    update server file, clean code

--- server.ts
+++ server.ts
@@ -1,11 +1,3 @@
-import { serveStatic } from 'hono/deno'
-
 import app from "./index.tsx"
-
-app.get('/assets/favicon.svg', (c) => {
-  return c.text('Icon is available');
-});
-
-app.use('/assets/*', serveStatic({ root: './assets' }))
 
 Deno.serve(app.fetch)
