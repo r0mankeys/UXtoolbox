637638cb5c65f1bc1265e5e94b67f6f76bff9934
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-11-16 21:50:01 +0000

    add character-set and set viewport to look good on all devices

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,6 +12,8 @@ const Layout: FC<LayoutProps> = (props) => {
     <!DOCTYPE html>
     <html>
       <head>
+        <meta charset="UTF-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${props.pageTitle}</title>
         <link rel="icon" href="/assets/favicon.svg" />
         <script src="https://cdn.twind.style"></script>
